ee77273d47e39b1a89a6d7f6bfdbf49c6fa9499b
shwetatiw <shwetatiwari20aug@gmail.com>
2017-04-05 10:42:27 +0530

    major project
    major project

---
+++ D_locker/app/src/main/java/trainedge/d_locker/FeedbackActivity.java
@@ -0,0 +1,20 @@
+package trainedge.d_locker;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
